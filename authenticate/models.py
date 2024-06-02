a898c80de7f57ba02985ec9488519f87aef6b3b0
Adivhaho Jeff <221649921@edu.vut.ac.za>
2024-06-02 23:01:11 +0200

    Fixed bugs in authenticate app

--- authenticate/models.py
+++ authenticate/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 
-class UserPrfoile(models.Model):
+class UserProfile(models.Model):
 	first_name = models.CharField(max_length=100 )
 	last_name = models.CharField(max_length=100)
-	email = models.CharField(max_length=100, unique=True)
+	email = models.EmailField(max_length=100, unique=True)
 	id_number = models.CharField(max_length=100, unique=True)
 	password = models.CharField(max_length=100)
 	
--- authenticate/urls.py
+++ authenticate/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 
 urlpatterns = [
-	path('sign_in/', views.sign_in, name="sign_in")
+	path('sign_in/', views.sign_in, name="sign_in"),
+	path('log_out/',views.log_out, name="log_out"),
+	path('sign_up/', views.sign_up, name="sign_up"),
+	path('', views.landing, name="landing")
 ]
