696546c9695d084ca328d13a44b175397ef22445
Adivhaho Jeff <221649921@edu.vut.ac.za>
2024-06-02 11:43:51 +0200

    Configured authenicate app and added a sign_in view route

--- authenticate/views.py
+++ authenticate/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+import json
 # Create your views here.
+
+
+def sign_in(request):
+
+	if request.method == "POST":
+
+		data = json.loads(request.body)
+		email = data.get('email')
+		password = data.get('password')
+
+
+
+	return render(request, "login.html")
